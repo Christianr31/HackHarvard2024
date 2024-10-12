a38382d5c670a749e3d54f5b36ce830a06199a7a
chris <rodriguez.christian0502@gmail.com>
2024-10-12 12:54:16 -0400

    button

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,35 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import Button from "./components/Button";
+import Alert from "./components/Button";
+
+import "./App.css"
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [alertVisible, setAlertVisibility] = useState(false);
+
+  const handleRecordClick = () => {
+    if (alertVisible != false) {
+      return;
+    }
+    setAlertVisibility(true);
+    console.log("Button clicked");
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div >
+      {alertVisible && <Alert color="secondary" onClick={() => setAlertVisibility(false)}> Stop </Alert>}
+      <Button color="primary" onClick={() => handleRecordClick()}>
+        Rec
+      </Button>
+    </div>
+  );
 }
 
-export default App
+export default App;
